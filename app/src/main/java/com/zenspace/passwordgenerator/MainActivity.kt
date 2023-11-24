2e5223c6f43f6578f1b364a6c69d5b61e4f408c2
Shivansh Kumar <codes.stalker@gmail.com>
2023-11-24 23:26:21 +0530

    Updated UI

--- app/src/main/java/com/zenspace/passwordgenerator/MainActivity.kt
+++ app/src/main/java/com/zenspace/passwordgenerator/MainActivity.kt
@@ -3,44 +3,92 @@ package com.zenspace.passwordgenerator
 import android.animation.ObjectAnimator
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.zenspace.passwordgenerator.databinding.ActivityMainBinding
 
-
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var finalCombination: String
+    private lateinit var generatedPassword: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
+
         val imageView: ImageView = binding.refresh
+        finalCombination = "qwertyuiopasdfghjklzxcvbnm"
+        generatedPassword = ""
 
         binding.refresh.setOnClickListener {
             rotateImage(imageView)
         }
 
-        setSliderStepsize()
+        setSliderStepSize()
 
         binding.passwordLength.addOnChangeListener { _, value, _ ->
-            setTheLengthOfPassword(value)
+            setTheLengthOfPasswordAndGenerate(value)
         }
 
+        checkDefaultCheckBox()
+
+        binding.generatePassword.setOnClickListener {
+            binding.password.text = generatePassword()
+        }
     }
 
-    private fun setTheLengthOfPassword(value: Float) {
+    private fun generatePassword(): String {
+        getDataFromCheckBox()
+        Log.i("8825", finalCombination)
+        return generateRandomPassword(finalCombination, binding.passwordLength.value.toInt())
+    }
+
+    private fun generateRandomPassword(finalCombination: String, length: Int): String {
+        val password = StringBuilder()
+        val random = java.util.Random()
+
+        repeat(length) {
+            val index = random.nextInt(finalCombination.length)
+            password.append(finalCombination[index])
+        }
+
+        return password.toString()
+    }
+
+    private fun checkDefaultCheckBox() {
+        binding.includeLowerCase.isChecked = true
+        binding.includeLowerCase.isClickable = false
+    }
+
+    private fun setTheLengthOfPasswordAndGenerate(value: Float) {
         binding.lengthIndicator.setTextColor(Color.BLACK)
-        binding.lengthIndicator.text = "Password Length = " + value.toInt().toString()
+        binding.lengthIndicator.text = "Password Length = ${value.toInt()}"
+        getDataFromCheckBox()
     }
 
-    private fun setSliderStepsize() {
+    private fun getDataFromCheckBox() {
+        if (binding.includeNumbers.isChecked) {
+            finalCombination += "1234567890"
+        }
+
+        if (binding.includeUpperCase.isChecked) {
+            finalCombination += "QWERTYUIOPASDFGHJKLZXCVBNM"
+        }
+
+        if (binding.includeSpecialCharacters.isChecked) {
+            finalCombination += "!@#$%^&*()_+~`=-,./\\|][}{'\"?><"
+        }
+    }
+
+    private fun setSliderStepSize() {
         binding.passwordLength.stepSize = 1.0f
     }
 
     private fun rotateImage(imageView: ImageView) {
         val rotateAnimator = ObjectAnimator.ofFloat(imageView, "rotation", 0f, 360f)
-        rotateAnimator.duration= 1000
+        rotateAnimator.duration = 1000
         rotateAnimator.start()
     }
 }
